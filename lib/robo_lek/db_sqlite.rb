6ce5a02e1ba6dba43e197a1a99ca4aabba4a8ced
Douglas Petronilio <dougpetronilio@gmail.com>
2013-11-07 11:14:22 -0200

    banco de dados pra testes.

--- lib/robo_lek/db_sqlite.rb
+++ lib/robo_lek/db_sqlite.rb
@@ -6,7 +6,7 @@ end
 module RoboLek
   
   def self.DBSqlite(db = nil)
-    db ||= SQLite3::Database.new "db/development.sqlite3" 
+    db ||= SQLite3::Database.new "db/test.sqlite3" 
     self::DBSqlite.new(db)
   end
   
