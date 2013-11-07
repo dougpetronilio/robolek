6719fc13632f547a3260ffd8d2a14966353f7eba
Douglas Petronilio <dougpetronilio@gmail.com>
2013-11-07 02:17:18 -0200

    erro na falta de sqlite3.

--- lib/robo_lek.rb
+++ lib/robo_lek.rb
@@ -2,7 +2,10 @@ require "robo_lek/trata_link"
 require "robo_lek/trata_produto"
 require "robo_lek/crawler"
 require "robo_lek/db_mongo"
-require "robo_lek/db_sqlite"
+begin
+  require "robo_lek/db_sqlite"
+rescue
+end
 require "robo_lek/db_postgres"
 require "robo_lek/pagina"
 require "robo_lek/produto"
